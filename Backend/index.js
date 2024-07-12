593d30c00885d7f2d8321798dab811a361f485f0
jatlashivakumar <166720675+jatlashivakumar@users.noreply.github.com>
2024-07-12 19:05:19 +0530

    Update index.js
    final changes

--- Backend/index.js
+++ Backend/index.js
@@ -9,26 +9,26 @@ import userRoute from "./route/user.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
 mongoose.connect(URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    // Define routes after successful connection
-    app.use("/book", bookRoute);
-    app.use("/user", userRoute);
-
-    // Start server
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}`);
-    });
   })
   .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+    console.log("Error connecting to MongoDB: ", error);
   });
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
